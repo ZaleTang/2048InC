ed4f7586c5eadd1aa616433d71d581a741385463
ZaleTang <ZaleTang@gmail.com>
2018-02-08 14:58:29 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,43 +1,53 @@
 #include <stdio.h>
 #include <time.h>
 #include <conio.h>
-
-void printaline(int,int,int,int);
-void printmatrix(int*);
-void deal(int,int*);
-
-int rand1();
-int getkey();
+#include "inc/deal.h"
 
 int main()
 {
-    int i,j,key;
+    int i,j,rf,rx,ry,key;
 
-    int a[4][4];
-    for(i=0;i<4;i++)
-        for(j=0;j<4;j++)
-            a[i][j]=0;
+    int a[4][4]={0,2,0,2,
+                 0,2,2,0,
+                 0,0,2,0,
+                 2,0,2,0};
+
     i=0;j=0;
-    a[2][3]=2;
-    a[2][2]=2;
 
-    printmatrix(&a);
+    printmatrix(a);
 
 
-    deal(getch(),&a);
-    printmatrix(&a);
+    while((key=getkey())!=27)
+    {
+        deal(key, a);
 
-    deal(getch(),&a);
-    printmatrix(&a);
+        rf=0;
+        for(i=0;i<4;i++)
+            for(j=0;j<4;j++)
+            {
+                if(a[i][j]==0)
+                    rf=1;break;
+            }
 
-    deal(getch(),&a);
-    printmatrix(&a);
+        if(rf==1)
+        {
+            while(rf==1)
+            {
+                rx=randlx(),ry=randly();
+                if(a[rx][ry]==0)
+                {
+                    a[rx][ry]=randn();
+                    rf=0;
+                }
+            }
+        }
 
-    deal(getch(),&a);
-    printmatrix(&a);
+        system("cls");
 
-    deal(getch(),&a);
-    printmatrix(&a);
+        printmatrix(a);
+        printf("\n");
+    }
+
 
 
 
@@ -54,7 +64,7 @@ void printaline(int a,int b,int c,int d)
 
 void printmatrix(int* martrix)
 {
-    system("cls");
+    //system("cls");
     printf("////////////////////////////////////////////////\n");
     printaline(*martrix,*(martrix+1),*(martrix+2),*(martrix+3));
     printaline(*(martrix+4),*(martrix+5),*(martrix+6),*(martrix+7));
@@ -65,118 +75,19 @@ void printmatrix(int* martrix)
 
 }
 
-int rand1()
+int getkey()
 {
-    srand((unsigned)time(NULL));
-    return rand();
+    /**键值
+
+    up 72  down 80  left 75   right 77
+
+    w 119 W 87   s 115 S83   a 97 A65   d 100 D68
+
+    *
+    */
+
+    if(getch()==224)
+        return getch();
+
 }
 
-//int getkey()
-//{
-//    char a;
-//    switch(getch())
-//    {
-//        case 72:a=1;break;
-//        case 80:a=2;break;
-//        case 75:a=3;break;
-//        case 77:a=4;break;
-//        case 27:a=5;break;
-//    }
-//
-//    return a;
-//}
-
-void deal(int key,int* martrix)
-{
-    int n,i,j,it,jt,at[4][4];
-
-
-    martrix--;
-    for(i=0;i<4;i++)
-        for(j=0;j<4;j++)
-        {
-            at[i][j]=*(martrix++);
-        }
-
-    //整体上移
-    if (key==72)
-    {
-        for(j=0;j<4;j++)
-        {
-            if (at[0][j] == 0 || at[0][j] == at[1][j])
-            {
-                at[0][j] = at[0][j] + at[1][j];
-                at[1][j] = at[2][j];
-                at[2][j] = at[3][j];
-                at[3][j] = 0;
-            }
-
-            if (at[1][j] == 0 || at[1][j] == at[2][j])
-            {
-                at[1][j] = at[1][j] + at[2][j];
-                at[2][j] = at[3][j];
-                at[3][j] = 0;
-            }
-
-            if (at[2][j] == 0 || at[2][j] == at[3][j])
-            {
-                at[2][j] = at[2][j] + at[3][j];
-                at[3][j] = 0;
-            }
-
-        }
-    }
-
-    //整体下移
-    if (key==2)
-    {
-        for(j=0;j<4;j++)
-            for(i=3;i>=0;i--)
-            {
-                while(at[i][j]==0)
-                {
-                    at[i][j]=at[i+1][j];
-                }
-
-                if(at[i][j]=at[i-1][j])
-                {
-                    at[i][j]=2*at[i][j];
-                    for(i=i-1;j>0;i--)
-                    {
-                        at[i][j]=at[i+1][j];
-                    }
-                }
-
-            }
-
-    }
-
-
-    if (key==3)
-    {
-        for(i=0;i<4;i++)
-            for(j=0;j<4;j++)
-            {
-                if(at[i][j]=at[i][j+1])
-                {
-                    at[i][j]=2*at[i][j];
-                    for(j=j+1;j<4;j++)
-                    {
-                        at[i][j]=at[i][j+1];
-                    }
-                }
-
-            }
-
-    }
-
-    martrix-=16;
-    for(i=0;i<4;i++)
-        for(j=0;j<4;j++)
-        {
-            *(martrix++)=at[i][j];
-        }
-
-
-
-}
